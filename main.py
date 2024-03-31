8e41e0fad6e9db29eedcc7ac30593393fb9e71a3
maryja-harshunova <spiakota@outlook.com>
2024-03-31 15:00:56 +0300

    set window

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 from pygame import *
+
+BGCOLOR = (230, 200, 89)
+TEXTCOLOR = (134, 74, 249)
+
+SCREENSIZE = (700, 700)
+
+window = display.set_mode(SCREENSIZE)
+display.set_caption("ping-pong")
+
+timer = time.Clock()
+
+run = True
+
+while run: 
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    
+    window.fill(BGCOLOR)
+    display.update()
+    timer.tick(60)
